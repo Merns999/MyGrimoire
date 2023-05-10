ab793a1b444e1d416e801196afebdb004bce88c1
Mansur Akidah <mansurakidah@gmail.com>
2023-05-10 21:42:36 +0300

    Entered a MyGrimoire domain model

---
+++ Grimoire/Models/Domain/MyGrimoire.cs
@@ -0,0 +1,14 @@
+﻿namespace Grimoire.Models.Domain
+{
+    public class MyGrimoire
+    {
+        public  Guid Id { get; set; }
+        public string Heading{ get; set; }
+        public string shortdescription { get; set; }
+        public string Content { get; set; }
+        public string ImgUrl { get; set; }
+        public string Urlhandle { get; set; }
+        public DateTime Released { get; set; }
+        public bool Visible { get; set; }
+    }
+}
